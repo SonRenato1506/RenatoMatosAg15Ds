197a5d8b2bec107935bac8d6461368f002310cfe
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2025-11-17 09:07:50 -0300

    Add files via upload

---
+++ Evento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppEventos.Models;
+
+public class Evento
+{
+    public string Nome { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataTermino { get; set; }
+    public int NumeroParticipantes { get; set; }
+    public string Local { get; set; }
+    public double CustoPorParticipante { get; set; }
+
+    public int DuracaoDias
+    {
+        get
+        {
+            var diff = DataTermino - DataInicio;
+            return (int)Math.Ceiling(diff.TotalDays);
+        }
+    }
+
+    public double CustoTotal => NumeroParticipantes * CustoPorParticipante;
+}
+
